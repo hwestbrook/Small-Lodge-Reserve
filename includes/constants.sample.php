d9c4775db1452dfdfafcddc13a2039e7883def7f
Heath <hwestbrook@gmail.com>
2010-07-18 09:10:53 -0700

    to add back the constants file as a sample
    Signed-off-by: Heath <hwestbrook@gmail.com>

---
+++ includes/constants.sample.php
@@ -0,0 +1,27 @@
+<?
+
+    /***********************************************************************
+     * constants.php
+     *
+     * Computer Science 50
+     * Problem Set 7
+     *
+     * Global constants.
+     **********************************************************************/
+
+
+    // your database's name
+    define("DB_NAME", "xxxxx");
+
+    // your database's username
+    define("DB_USER", "xxxxx");
+
+    // your database's password
+    define("DB_PASS", "xxxxx");
+
+    // hostname of course's database server
+    define("DB_SERVER", "xxxxx.com");
+	
+	// webmaster email address
+	define("WEBMASTER", "xxxxxxxxx");
+?>
